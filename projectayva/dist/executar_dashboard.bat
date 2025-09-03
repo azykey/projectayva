3e73a9dbb14cd92a7def8928754923d67bdf20d2
Key <adilsonoliveira.2788@gmail.com>
2025-09-03 16:51:13 -0300

    Delete projectayva directory

--- projectayva/dist/executar_dashboard.bat
+++
@@ -1,4 +0,0 @@
-@echo off 
-echo Iniciando Adilson Dashboard... 
-adilson_dashboard.exe 
-pause 
--- projectayva/janela.h
+++
@@ -1,6 +0,0 @@
-/*
- * Propriedade Intelectual: Adilson Oliveira 2025
- * Código original e protegido
- */
-#pragma once
-void abrirJanela(); 
